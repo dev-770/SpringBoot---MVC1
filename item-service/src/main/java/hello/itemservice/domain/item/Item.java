dbb9b15a3fd0f156325b8e1acf653e0136625a9d
unknown <rokap11111@gmail.com>
2021-12-28 23:49:01 +0900

    SpringBoot - MVC1

--- item-service/src/main/java/hello/itemservice/domain/item/Item.java
+++ item-service/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,12 +1,13 @@
 package hello.itemservice.domain.item;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class Item {
 
@@ -14,8 +15,5 @@ public class Item {
 	private String itemName;
 	private Integer price;
 	private Integer quantity;
-	
-	public Item() {
-	}
 	
 }
